d5fa4b2b053182f381f814252048d63204ec21c2
mauro287 <mr.naulaq2@uea.edu.ec>
2025-01-12 12:45:16 -0600

    semana 4

--- semana 4/ejemplo.py
+++ semana 4/ejemplo.py
@@ -7,5 +7,12 @@ class Producto:
     def descontar_precio(self, porcentaje):
         self.precio *= (1 - porcentaje/100)
 
+    def calcular_precio_total(self, cantidad):
+        return self.precio * cantidad
+
 # Creando un objeto Producto para el arroz
 arroz = Producto("Arroz", 20, "Una arroba de arroz")
+
+# Calculando el precio de 2 arrobas de arroz
+precio_total = arroz.calcular_precio_total(2)
+print("El precio total de 2 arrobas de arroz es:", precio_total)
